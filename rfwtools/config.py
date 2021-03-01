20966af962992e27299db61731fba977d2d3a03f
Adam Carpenter <adamc@jlab.org>
2021-03-01 14:00:33 -0500

    Updated documentation / typehints throughout

--- rfwtools/config.py
+++ rfwtools/config.py
@@ -1,9 +1,54 @@
+"""This module is used for managing the configuration of rfwtools.
+
+This module contains as single class, a Singleton, that manages the configuration of many parts of rfwtools.  This
+includes the definition of several resource locations, debug behavior, and filters on valid data.
+
+Basic Usage:
+
+::
+  from rfwtools.config import Config
+  config = Config()
+  config.output_dir = '/path/to/my/save/files/'
+
+  # Or equivalently
+
+  Config().output_dir = '/path/to/my/save/files/'
+"""
+
 import os
+from typing import Any
+
 import yaml
 
 
 class Config:
-    """A singleton class for containing application configuration.  Written as a singleton to enable easy extension"""
+    """A singleton class for containing application configuration.  Written as a singleton to enable easy extension.
+
+    Attributes:
+        instance:
+            The internal configuration object.
+        app_dir:
+            A string defining the root directory of the application.  Currently used in initial setup only.  Defaults
+            to the current working directory.
+        data_dir:
+            A string defining the path to waveform data.  Defaults to CWD/data/waveforms/data/rf.
+        debug:
+            A boolean that controls debug output.  Defaults to False (meaning no debug output).
+        label_dir:
+            A string defining the directory that holds label files. Defaults to CWD/data/labels
+        output_dir:
+            A string defining the directory to read/write saved files produced by this package.  Defaults to
+            CWD/processed-output.
+        exclude_zones:
+            A list of strings that define zones to exclude from parsing operations.  Defaults to ['0L04']
+        exclude_times:
+            A list of 2-tuples of datetime.datetime objects.  Each tuple is a range of time to exclude from analysis.
+            Both endpoints are inclusive, and a value of  None is interpreted as +/- Inf.  Defaults to None.
+        data_server:
+            A string for the server hostname to contact for the web-based data API.  Defaults to accweb.acc.jlab.org.
+        wfb_base_url:
+            A string that is the base URL (context root) of the waveform browser web app.  Defaults to wfbrowser.
+    """
 
     class __Config:
         """This private inner class is used to implement the singleton interface"""
@@ -49,17 +94,36 @@ class Config:
                 Config.read_config_file()
 
     @staticmethod
-    def dump_yaml_string():
-        """Write config out to a YAML formatted string.  Workaround - the nested Class causes trouble with pickle."""
+    def dump_yaml_string() -> str:
+        """Write config out to a YAML formatted string.
+
+        Note: the nested Class causes trouble with pickle so this is a reasonable alternative in most scenarioes.
+
+        Returns:
+            The YAML-formatted configuration string.
+        """
         return yaml.dump(Config.instance.__dict__)
 
     @staticmethod
-    def load_yaml_string(string):
-        Config.instance.__dict__ = yaml.safe_load(string)
+    def load_yaml_string(string: str) -> None:
+        """Read in a YAML formatted string containing configuration information.
+
+        This method overwrites only the values defined in the string.
+
+        Arguments:
+            string: The YAML-formatted string to parse.
+        """
+        cfg = yaml.safe_load(string)
+        for key in cfg.keys():
+            Config.instance.__dict__[key] = cfg[key]
 
     @staticmethod
-    def write_config_file(file):
-        """Writes out the current configuration to the specified file."""
+    def write_config_file(file: str) -> None:
+        """Writes out the current configuration to the specified file.
+
+        Arguments:
+            file: The name file the name to write configuration infomration to.
+        """
 
         # Make sure the singleton config exists
         Config()
@@ -67,10 +131,15 @@ class Config:
             f.write(Config.dump_yaml_string())
 
     @staticmethod
-    def read_config_file(file=None):
-        """Parses a YAML-formatted config file.  Defaults to rfwtools.cfg in the current working directory.
+    def read_config_file(file: str = None) -> None:
+        """Parses a YAML-formatted config file and updates internal configuration.
 
         Relative files will be considered relative to the current working directory.
+
+        Arguments:
+            file:
+                Path to the file to read.  Relative paths are considered as relative to the current working directory.
+                Default value is rfwtools.cfg
         """
 
         if file is None:
@@ -86,10 +155,10 @@ class Config:
         with open(path, mode="r") as f:
             Config.load_yaml_string(f.read())
 
-    def __getattr__(self, name):
-        """Redirect unresolved attribute queries to the single instance"""
+    def __getattr__(self, name: str) -> Any:
+        """Redirect unresolved attribute queries to the single instance."""
         return getattr(Config.instance, name)
 
-    def __setattr__(self, name, value):
-        """Redirect attribute modification to the single instance"""
+    def __setattr__(self, name: str, value: Any) -> None:
+        """Redirect attribute modification to the single instance."""
         return setattr(Config.instance, name, value)
