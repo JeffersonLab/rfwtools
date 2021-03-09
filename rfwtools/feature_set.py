6b009523ffdd336a878aef3f543c03ba885410e4
Adam Carpenter <adamc@jlab.org>
2021-03-09 11:06:22 -0500

    Documentation improvements, remove unused code.

--- rfwtools/feature_set.py
+++ rfwtools/feature_set.py
@@ -3,7 +3,9 @@
 Typically a FeatureSet is created by DataSet.produce_feature_set().  Once created the class provides some tools for
 easy dimensionality reduction and visualization.
 
-Basic Usage:
+Basic Usage Examples:
+
+Creation through a DataSet workflow:
 ::
 
     from rfwtools.data_set import DataSet
@@ -16,10 +18,23 @@ Basic Usage:
     fs = ds.feature_set
     fs.do_pca_reduction(n_components=10)
     fs.display_2d_scatterplot(query="zone=='1L25'", title="1L25 PCA subset")
+    fs.save_csv("my_feature_set.csv")
+
+Loading from file:
+::
+    from rfwtools.feature_set import FeatureSet
+
+    fs = FeatureSet()
+    fs.load_csv("my_feature_set.csv")
+    fs.do_pca_reduction()
+    fs.display_2d_scatterplot(query="zone=='1L25'", title="1L25 PCA subset")
+
 """
 
 
 import os
+from typing import List, Tuple
+
 import pandas as pd
 
 from rfwtools.dim_reduction import pca
@@ -34,32 +49,35 @@ class FeatureSet(ExampleSet):
     This class is an extension on ExampleSet meant to handle additional analysis operations.
     """
 
-    def __init__(self, df=None, filename=None, in_dir=None, sep=",", name="", metadata_columns=None):
-        """Construct a FeatureSet.  Use the supplied DataFrame, or load filename if no df argument is given
+    def __init__(self, df: pd.DataFrame = None, filename: str = None, in_dir: str = None, sep: str = ",",
+                 name: str = "", metadata_columns: List[str] = None):
+        """Construct a FeatureSet.  Can use either a DataFrame or CSV-like file.
 
-        Args:
-            df (DataFrame): A DataFrame containing the FeatureSet data to include.  The following columns must be
-                            included - ['zone', 'dtime', 'cavity_label', 'fault_label', 'label_source'].  Any
-                            additional columns will be treated as the features.  Note: A copy of df is saved.
-            filename (str) - The filename to save.  Will be relative in_dir
-            in_dir (str) - The directory to find the file in.  Defaults to Config().output_dir
-            sep (str) - Delimiter string used by Pandas to parse given "csv" file
-            name (str): A string that may be used to help identify this FeatureSet in plot titles, etc.
-            metadata_columns (list(str)): A list of the names of the columns that are metadata (e.g., "zone" or
-                                          "cavity_label").  If None, a standard set is assumed.
+        Arguments:
+            df:
+                A DataFrame containing the FeatureSet data to include.  The following columns must be included -
+                ['zone', 'dtime', 'cavity_label', 'fault_label', 'label_source'].  Any additional columns will be
+                treated as the features.  Note: A copy of df is saved.
+            filename:
+                The filename to load.  Will be relative to in_dir
+            in_dir:
+                The directory to find the CSV-like file in.  Defaults to Config().output_dir
+            sep:
+                Delimiter string used by Pandas to parse given CSV-like file
+            name:
+                A string that may be used to help identify this FeatureSet
+            metadata_columns:
+                A list of the names of the columns that are metadata (e.g., "zone" or "cavity_label").  Any supplied
+                column names are in addition to the mandatory columns for an ExampleSet.  metadata_columns are treated
+                as required columns.
         """
 
         # Setup the Example
         super().__init__()
 
-        if metadata_columns is None:
-            # Use the required columns of the parent ExampleSet.  Since no extra required columns we can leave
-            # self._req_columns alone
-            self.metadata_columns = self.get_required_columns()
-        else:
-            # Here we're assuming that all of the metadata columns should be required
-            self.metadata_columns = self.get_required_columns() + metadata_columns
-            self._req_columns = metadata_columns
+        #: These columns are required in internal DataFrames and are excluded from analysis routines.
+        self.metadata_columns = None
+        self._update_metadata_columns(metadata_columns=metadata_columns)
 
         if df is not None:
             # Make sure the DataFrame has the metadata columns.  The
@@ -75,6 +93,7 @@ class FeatureSet(ExampleSet):
             # Make a standard column order.  All metadata columns first, followed by feature columns
             df = df[self.metadata_columns + df.drop(columns=self.metadata_columns).columns.to_list()]
 
+            # Import the DataFrame into the ExampleSet's internal DataFrame
             self.update_example_set(df)
 
         elif filename is not None:
@@ -83,41 +102,32 @@ class FeatureSet(ExampleSet):
         # A brief human friendly name for this FeatureSet
         self.name = name
 
-        # The pca reduced feature data, one example per row
+        #: The pca reduced feature data, one example per row
         self.__pca_df = None
 
-        # The pca model.  Either None or is the fitted sklearn PCA object.  This is left publicly accessible so users
-        # have access for custom analysis or visualization (e.g., transforming future examples).  Users beware
-        # modifying this!
+        #: The pca model.  Either None or is the fitted sklearn PCA object.  This is left publicly accessible so users
+        #: have access for custom analysis or visualization (e.g., transforming future examples).  Users beware
+        #: modifying this!
         self.pca = None
 
-    # TODO Delete this comment block!
-    # def save_csv(self, filename, out_dir=None, sep=','):
-    #     """Write out the FeatureSet data as a CSV file relative to out_dir.
-    #
-    #     Args:
-    #         filename (str) - The filename to save.  Will be relative out_dir
-    #         out_dir (str) - The directory to save the file in.  Defaults to Config().output_dir
-    #         sep (str) - Delimiter string used by Pandas to parse given "csv" file
-    #     """
-    #     if out_dir is None:
-    #         out_dir = Config().output_dir
-    #     self._example_df.to_csv(os.path.join(out_dir, filename), sep=sep, index=False)
-
-    def load_csv(self, filename, in_dir=None, sep=',', metadata_columns=None):
+    def load_csv(self, filename: str, in_dir: str = None, sep: str = ',', metadata_columns: List[str] = None) -> None:
         """Read in a CSV file that has FeatureSet data.  Relative to in_dir if filename is str.
 
-        Args:
-            filename (str) - The filename to save.  Will be relative out_dir
-            in_dir (str) - The directory to find the file in.  Defaults to Config().output_dir
-            sep (str) - Delimiter string used by Pandas to parse given "csv" file
-            metadata_columns (list) - A list of column names to treat as metadata.  This updates the FeatureSet's list.
+        Arguments:
+            filename:
+                The filename to load.  Will be relative in_dir
+            in_dir:
+                The directory to find the file in.  Defaults to Config().output_dir
+            sep:
+                Delimiter string used by Pandas to parse given "csv" file
+            metadata_columns:
+                A list of column names to treat as metadata.  This updates the FeatureSet's list.  No changes are made
+                if it is None.
         """
         
         # Update the column info if indicated
         if metadata_columns is not None:
-            self.metadata_columns = metadata_columns
-            self._req_columns = self.metadata_columns
+            self._update_metadata_columns(metadata_columns=metadata_columns)
 
         # Clear the dimensionality reduction attributes
         self.__pca_df = None
@@ -126,31 +136,58 @@ class FeatureSet(ExampleSet):
         # Load it into the parent's ExampleSet _example_df
         super().load_csv(filename=filename, in_dir=in_dir, sep=sep)
 
-    def get_pca_df(self):
-        """Return a copy of the PCA reduction as a DataFrame.  Will be None if the reduction has not been done."""
+    def _update_metadata_columns(self, metadata_columns: List[str]) -> None:
+        """This updates the metadata columns and alters other related data structures."""
+        if metadata_columns is None:
+            # Use the required columns of the parent ExampleSet.  Since no extra required columns we can leave
+            # self._req_columns alone
+            self.metadata_columns = self.get_required_columns()
+        else:
+            # Here we're assuming that all of the metadata columns should be required
+            self.metadata_columns = self.get_required_columns() + metadata_columns
+            self._req_columns = metadata_columns
+
+    def get_pca_df(self) -> pd.DataFrame:
+        """Get a copy of the PCA reduction as a DataFrame.  Will be None if the reduction has not been done.
+
+        Each example is on it's own row with it's primary components.
+
+        Returns:
+            A copy of the PCA results along with the example's metadata.
+        """
         if self.__pca_df is None:
             return None
         else:
             return self.__pca_df.copy()
 
-    def update_feature_df(self, df):
-        """Updates the feature_df and blanks other internal data derived from it"""
-        # TODO change this to self.update_example_df(df)
-        self._example_df = df.copy()
+    def update_example_df(self, df: pd.DataFrame) -> None:
+        """Update the _example_df and blanks other internal data derived from it.
+
+        Arguments:
+            df:
+                A DataFrame containing an example per row with additional feature information.  Must be valid for this
+                FeatureSet.
+        """
+        super().update_example_set(df)
         self.__pca_df = None
 
-    def do_pca_reduction(self, metadata_cols=None, report=True, n_components=3, **kwargs):
-        """Performs PCA on subset of columns of feature_df and maintains some example info in results.
+    def do_pca_reduction(self, metadata_cols: List[str] = None, report: bool = True, n_components: float = 3,
+                         **kwargs) -> None:
+        """Perform PCA on subset of columns of example_df and maintain example metadata in results.
 
-        Args:
-            metadata_cols (list(str)) - The column names of feature_df that contain the metadata of the events (labels,
-                                        etc.).  All columns not listed in event_cols are used in PCA analysis.  If None,
-                                        it defaults to the values supplied at construction.
-            report (bool) - Should a report of explained variance be printed
-            n_components (int) - The number of principal components to calculate.
-            **kwargs (dict) - Remaining keyword arguments will be passed to sklearn.decomposition.PCA
+        The results are accessible through the get_pca_df() method and the fitted model through the pca attribute.
 
-        Returns (PCA): Returns the PCA object after performing fit_transform()
+        Arguments:
+            metadata_cols:
+                The column names of feature_df that contain the metadata of the events (labels, etc.).  All columns not
+                listed in event_cols are used in PCA analysis.  If None, it defaults to the values supplied at
+                construction of FeatureSet.
+            report:
+                Should a report of explained variance be printed
+            n_components:
+                The number of principal components to calculate.
+            **kwargs:
+                Remaining keyword arguments will be passed to sklearn.decomposition.PCA
         """
         # Setup data for doing PCA.  x is the feature data to be reduced
         if metadata_cols is None:
@@ -166,21 +203,21 @@ class FeatureSet(ExampleSet):
             print("Explained Variance Ratio")
             print(self.pca.explained_variance_ratio_)
 
-    def display_2d_scatterplot(self, technique="pca", alpha=0.8, s=25, title=None, figsize=(12, 12), query=None,
-                               **kwargs):
+    def display_2d_scatterplot(self, technique: str = "pca", alpha: float = 0.8, s: int = 25, title: str = None,
+                               figsize: Tuple[int, int] = (12, 12), query: str = None, **kwargs) -> None:
         """Display a two-dimensional scatterplot of the dimensionally reduced feature set.
 
         If the type specified has not already been generated, an exception is raised.  Note: consider passing
-        hue=<pd.Series> and/or style=<pd.Series> to control which column colors/styles each point.
+        hue=<pca_df column_name> and/or style=<pca_df column_name> to control which column colors/styles each point.
 
-        Args:
-            technique (str): The type of dim reduction data to display.  Currently the only supported option is pca.
-            alpha (float): Controls point transparency
-            s (int): Controls point size
-            title (str): The title of the scatterplot
-            figsize (2-tuple(float)): The two dimensions of the size of the figure.  Passed to plt.figure.
-            query (str): A pd.DataFrame.query() expr argument.  Used to subset the data prior to plotting.
-            **kwargs (dict): All remaining parameters are passed directly to the scatterplot command
+        Arguments:
+            technique: The type of dim reduction data to display.  Currently the only supported option is pca.
+            alpha: Controls point transparency
+            s: Controls point size
+            title: The title of the scatterplot
+            figsize: The two dimensions of the size of the figure.  Passed to plt.figure.
+            query: A pd.DataFrame.query() expr argument.  Used to subset the data prior to plotting.
+            **kwargs: All remaining parameters are passed directly to the scatterplot command
         """
 
         # Figure out which type of plot we're showing
@@ -197,6 +234,7 @@ class FeatureSet(ExampleSet):
         scatterplot.scatterplot(data=df, x=x, y=y, title=title, alpha=alpha, s=s, figsize=figsize, **kwargs)
 
     def __select_technique_data(self, technique):
+        """Convenience method for getting the x,y plotting columns and total dataset for a given dim reduction."""
         if technique == "pca":
             if self.__pca_df is None:
                 raise RuntimeError("Internal pca_df is None.  Must first run do_pca_reduction().")
@@ -228,55 +266,3 @@ class FeatureSet(ExampleSet):
             return False
 
         return True
-
-    @classmethod
-    def __standardize_df_format(cls, df, zones=None, cavity_labels=None, fault_labels=None):
-        """Attempts to put a DataFrame in a 'standard' format.
-
-        This affects IN-PLACE variables that should categoricals, datetime, float, etc. and creates the example column
-        if not already present.  Columns are reordered.
-
-        Args:
-            df (pd.DataFrame) - The DataFrame to reformat
-
-        Returns: None
-        """
-
-        # Seems like the datetime dtype doesn't want to stick
-        df['dtime'] = df['dtime'].astype('datetime64[ns]')
-
-        # Update the dtypes so that we get categories, etc. where it makes sense
-        df['zone'] = df['zone'].astype('category')
-        df['fault_label'] = df['fault_label'].astype('category')
-        df['cavity_label'] = df['cavity_label'].astype('str')
-        df['cavity_label'] = df['cavity_label'].astype('category')
-
-        # Figure out what categories to enforce
-        if zones is None:
-            z = cls._known_zones
-        else:
-            z = zones
-        if cavity_labels is None:
-            cl = cls._known_cavity_labels
-        else:
-            cl = cavity_labels
-        if fault_labels is None:
-            fl = cls._known_fault_labels
-        else:
-            fl = fault_labels
-
-        # Ensure a consistent set of category levels and their order.
-        master = {
-            'zone': z,
-            'fault_label': fl,
-            'cavity_label': cl
-        }
-
-        # Add any missing levels and the make sure they are in a predictable order
-        for factor in master.keys():
-            for f in master[factor]:
-                # Add the category if it is not present
-                if f not in df[factor].cat.categories.values:
-                    df[factor] = df[factor].cat.add_categories(f)
-            # Enforce a known ordering
-            df[factor] = df[factor].cat.reorder_categories(sorted(df[factor].cat.categories))
