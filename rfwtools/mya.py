cd3caa08cdbf1f5e780ce5eebb96c23fa2a31aa4
Adam Carpenter <adamc@jlab.org>
2021-03-09 11:24:44 -0500

    Documentation updates

--- rfwtools/mya.py
+++ rfwtools/mya.py
@@ -1,3 +1,21 @@
+"""This module contains functions for interacting with the MYA EPICS archiver.
+
+These methods require access to an internal JLab network (onsite, VPN, etc.).
+
+Basic Usage Example:
+::
+
+    from rfwtools.mya import get_pv_value
+    from datetime import datetime
+    import sys
+
+    try:
+        val = get_pv_value('R1M1GSET', datetime(year=2019, month=11, day=13, hour=11))
+    except ValueError as ex:
+        print(f"Error retrieving archived value. {ex}", file=sys.stderr)
+"""
+from typing import Any
+
 import requests
 from rfwtools.network import SSLContextAdapter
 from datetime import datetime
@@ -6,16 +24,16 @@ __myquery_url__ = "https://myaweb.acc.jlab.org/myquery"
 """str: The base URL of the myquery web service.  Note: this is the internal service URL."""
 
 
-def get_json(url):
+def get_json(url: str) -> dict:
     """Simple function for making an HTTP GET request that should return a valid JSON content-type.
 
     This method creates a custom SSLContextAdapter that has access to the system's trusts CA certificates.
 
-    Args:
-        url (str): The URL on which to perform the HTTP GET
+    Arguments:
+        url: The URL on which to perform the HTTP GET
 
     Returns:
-        dict: A dictionary object representing the JSON response
+        A dictionary object representing the JSON response
 
     Raises:
         ValueError: If the URL returns a non-200 status code or if the response is not valid JSON content
@@ -36,19 +54,19 @@ def get_json(url):
     return r.json()
 
 
-def get_pv_value(PV, datetime, deployment='ops'):
-    """Method for performing a point-type myquery mayweb request.  Returns the only the PV value.
+def get_pv_value(PV: str, datetime: datetime, deployment: str = 'ops') -> str:
+    """Method for performing a point-type myquery myaweb request.  Returns the only the PV value.
 
     Args:
-        PV (str): The EPICS channel to look up
-        datetime (datetime): A datetime object representing the point in time for which the query should be performed
-        deployment(str): The name of a valid MYA deployment (defaults to 'ops')
+        PV: The EPICS channel to look up
+        datetime: A datetime object representing the point in time for which the query should be performed
+        deployment: The name of a valid MYA deployment (defaults to 'ops', other useful ones are 'hist', and 'dev')
 
     Returns:
         str: The archived value of PV at datetime according to MYA deployment deployment
 
     Raises:
-        ValueError: If the myquery point service returns an error response.
+        ValueError: If the myquery point service returns an HTTP error response.
     """
     timestamp = datetime.strftime("%Y-%m-%d+%H:%M:%S.%f")
     query = "/point?c={}&t={}&m={}&f=&v=".format(PV, timestamp, deployment)
