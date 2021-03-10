8f3b0489e53bfe1cc70fc9694f0ae778d0984e77
Adam Carpenter <adamc@jlab.org>
2021-03-10 10:38:43 -0500

    Documentation updates.

--- rfwtools/visualize/timeline.py
+++ rfwtools/visualize/timeline.py
@@ -1,13 +1,16 @@
+"""This module provides customized swarmplot visualizations."""
+
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
-def swarm_timeline(data, **kwargs):
+def swarm_timeline(data: pd.DataFrame, **kwargs) -> None:
     """Generate and display a swarmplot of fault/cavity label pairs over time.  Extra kwargs passed to swarmplot.
 
-    Note: kwargs especially usefull for modifying marker size with s=<int>.
+    Note: kwargs especially useful for modifying marker size with s=<int>.
 
-    Args:
-        data (DataFrame) - Expects a DataFrame with the format from ExampleSet
+    Arguments:
+        data: Expects a DataFrame with the format from ExampleSet
     """
     plt.figure(figsize=(20, 9))
     ax = sns.swarmplot(x=data.cavity_label, y=data['dtime'], hue=data.fault_label, **kwargs)
