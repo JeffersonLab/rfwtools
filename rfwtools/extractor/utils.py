d89bb50912000ddef7d0ec9748f55d86fb2bce86
Adam Carpenter <adamc@jlab.org>
2021-03-10 09:11:41 -0500

    Documentation updates.

--- rfwtools/extractor/utils.py
+++ rfwtools/extractor/utils.py
@@ -1,11 +1,18 @@
-def get_example_data(ex, query):
-    """Get the requested data from the provided Example.
+"""Utility functions to help with feature extraction."""
 
-    Args:
-        ex (Example) - The Example object
-        query (str) - The query parameter to apply to the Example.event_df or None if no query is requested
+from rfwtools.example import Example
+import pandas as pd
 
-    Returns (DataFrame) - The requested Example's data
+
+def get_example_data(ex: Example, query: str) -> pd.DataFrame:
+    """Get the requested data from the provided Example.  Handles loading and unloading of Example data.
+
+    Arguments:
+        ex: The Example object
+        query: The query parameter to apply to the Example.event_df or None if no query is requested
+
+    Returns:
+         The requested Example's event_df DataFrame
     """
     # Get the data
     ex.load_data()
